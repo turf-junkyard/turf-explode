dde9fa8137bc20c6188b37330b86a8b6abc84873
morganherlocker <morgan.herlocker@gmail.com>
2014-12-10 23:38:09 -0500

    semicolons

--- index.js
+++ index.js
@@ -1,99 +1,99 @@
-var flatten = require('flatten')
-var featureCollection = require('turf-featurecollection')
-var point = require('turf-point')
+var flatten = require('flatten');
+var featureCollection = require('turf-featurecollection');
+var point = require('turf-point');
 
 module.exports = function(fc){
   if(fc.type === 'FeatureCollection'){
     for(var i in fc.features){
-      var coordinates 
+      var coordinates ;
       switch(fc.features[i].geometry.type){
         case 'Point':
-          coordinates = [fc.features[i].geometry.coordinates]
+          coordinates = [fc.features[i].geometry.coordinates];
           break
         case 'LineString':
-          coordinates = fc.features[i].geometry.coordinates
+          coordinates = fc.features[i].geometry.coordinates;
           break
         case 'Polygon':
-          coordinates = fc.features[i].geometry.coordinates
-          coordinates = flatCoords(coordinates)
+          coordinates = fc.features[i].geometry.coordinates;
+          coordinates = flatCoords(coordinates);
           break
         case 'MultiPoint':
-          coordinates = fc.features[i].geometry.coordinates
+          coordinates = fc.features[i].geometry.coordinates;
           break
         case 'MultiLineString':
-          coordinates = fc.features[i].geometry.coordinates
-          coordinates = flatCoords(coordinates)
+          coordinates = fc.features[i].geometry.coordinates;
+          coordinates = flatCoords(coordinates);
           break
         case 'MultiPolygon':
-          coordinates = fc.features[i].geometry.coordinates
-          coordinates = flatCoords(coordinates)
+          coordinates = fc.features[i].geometry.coordinates;
+          coordinates = flatCoords(coordinates);
           break
       }
       if(!fc.features[i].geometry && fc.features[i].properties){
-        return new Error('Unknown Geometry Type')
+        return new Error('Unknown Geometry Type');
       }
     }
       
-    var exploded = featureCollection([])
+    var exploded = featureCollection([]);
 
     coordinates.forEach(function(coords){
-      exploded.features.push(point(coords[0], coords[1]))
+      exploded.features.push(point(coords[0], coords[1]));
     })
 
-    return exploded
+    return exploded;
   }
   else{
-    var coordinates 
-    var geometry
+    var coordinates ;
+    var geometry;
     if(fc.type === 'Feature'){
-      geometry = fc.geometry
+      geometry = fc.geometry;
     }
     else{
-      geometry = fc
+      geometry = fc;
     }
     switch(geometry.type){
       case 'Point':
-        coordinates = [geometry.coordinates]
+        coordinates = [geometry.coordinates];
         break
       case 'LineString':
-        coordinates = geometry.coordinates
+        coordinates = geometry.coordinates;
         break
       case 'Polygon':
-        coordinates = geometry.coordinates
-        coordinates = flatCoords(coordinates)
+        coordinates = geometry.coordinates;
+        coordinates = flatCoords(coordinates);
         break
       case 'MultiPoint':
-        coordinates = geometry.coordinates
+        coordinates = geometry.coordinates;
         break
       case 'MultiLineString':
-        coordinates = geometry.coordinates
-        coordinates = flatCoords(coordinates)
+        coordinates = geometry.coordinates;
+        coordinates = flatCoords(coordinates);
         break
       case 'MultiPolygon':
-        coordinates = geometry.coordinates
-        coordinates = flatCoords(coordinates)
+        coordinates = geometry.coordinates;
+        coordinates = flatCoords(coordinates);
         break
     }
     if(!geometry){
-      return new Error('No Geometry Found')
+      return new Error('No Geometry Found');
     }
 
-    var exploded = featureCollection([])
+    var exploded = featureCollection([]);
 
     coordinates.forEach(function(coords){
-      exploded.features.push(point(coords[0], coords[1]))
+      exploded.features.push(point(coords[0], coords[1]));
     })
 
-    return exploded
+    return exploded;
   }
 }
 
 function flatCoords(coords){
-  var newCoords = []
-  coords = flatten(coords)
+  var newCoords = [];
+  coords = flatten(coords);
   coords.forEach(function(c, i){
     if(i % 2 == 0) // if is even
-    newCoords.push([c, coords[i+1]])
+      newCoords.push([c, coords[i+1]]);
   })
-  return newCoords
+  return newCoords;
 }
--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-var test = require('tape')
-var polygon = require('turf-polygon')
-var point = require('turf-point')
-var featurecollection = require('turf-featurecollection')
-var explode = require('./')
+var test = require('tape');
+var polygon = require('turf-polygon');
+var point = require('turf-point');
+var featurecollection = require('turf-featurecollection');
+var explode = require('./');
 
 test('explode', function(t){
-  var poly = polygon([[[0,0], [0,10], [10,10] , [10,0]]])
+  var poly = polygon([[[0,0], [0,10], [10,10] , [10,0]]]);
   var p1 = point(0,0),
       p2 = point(0,10),
       p3 = point(10,10),
-      p4 = point(10,0)
-  var fc = featurecollection([p1,p2,p3,p4])
+      p4 = point(10,0);
+  var fc = featurecollection([p1,p2,p3,p4]);
 
-  var exploded = explode(poly)
+  var exploded = explode(poly);
 
-  t.ok(exploded.features, 'should take a feature or feature collection and return all vertices')
-  t.deepEqual(exploded, fc)
+  t.ok(exploded.features, 'should take a feature or feature collection and return all vertices');
+  t.deepEqual(exploded, fc);
 
-  t.end()
+  t.end();
 })
