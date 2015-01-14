27c0a6ee281feb4814b544477fea8d70dc1b30e6
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-13 16:35:04 -0800

    Change example from Null Island to Nadi, Fiji

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ var point = require('turf-point');
  * @param {GeoJSON} input input features
  * @return {FeatureCollection} a FeatureCollection of Point features representing the exploded input features
  * @example
- * var poly = turf.polygon([
- *   [[0,0], [0,10], [10,10] , [10,0], [0, 0]]], {});
- * //=poly
+ * var poly = turf.polygon([[
+ *  [177.396755, -17.795112],
+ *  [177.422161, -17.783506],
+ *  [177.439155, -17.799851],
+ *  [177.426624, -17.826164],
+ *  [177.404651, -17.836459],
+ *  [177.385425, -17.812926],
+ *  [177.381134, -17.797563],
+ *  [177.396755, -17.795112]
+ * ]]);
+ *
  * var points = turf.explode(poly);
  *
  * //=poly
