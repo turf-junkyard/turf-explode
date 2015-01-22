7a8a09681cc9a160eceaf82d217b59f59761dbaf
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-21 16:59:01 -0800

    Conform to turf doc style guide

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ var point = require('turf-point');
 
 /**
  * Takes any {@link GeoJSON} object and return all positions as
- * a collection of {@link Point|Points}.
+ * a {@link FeatureCollection} of {@link Point} features.
  *
  * @module turf/explode
  * @param {GeoJSON} input input features
- * @return {FeatureCollection} a FeatureCollection of Point features representing the exploded input features
+ * @return {FeatureCollection} a FeatureCollection of {@link Point} features representing the exploded input features
  * @throws {Error} if it encounters an unknown geometry type
  * @example
  * var poly = turf.polygon([[
- *  [177.396755, -17.795112],
- *  [177.422161, -17.783506],
- *  [177.439155, -17.799851],
- *  [177.426624, -17.826164],
- *  [177.404651, -17.836459],
- *  [177.385425, -17.812926],
- *  [177.381134, -17.797563],
- *  [177.396755, -17.795112]
+ *  [177.434692, -17.77517],
+ *  [177.402076, -17.779093],
+ *  [177.38079, -17.803937],
+ *  [177.40242, -17.826164],
+ *  [177.438468, -17.824857],
+ *  [177.454948, -17.796746],
+ *  [177.434692, -17.77517]
  * ]]);
  *
  * var points = turf.explode(poly);
