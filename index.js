6217cf791002142309f9fac3564dc4b43155828b
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-03 16:51:37 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -11,15 +11,22 @@ var point = require('turf-point');
  * @return {FeatureCollection} a FeatureCollection of {@link Point} features representing the exploded input features
  * @throws {Error} if it encounters an unknown geometry type
  * @example
- * var poly = turf.polygon([[
- *  [177.434692, -17.77517],
- *  [177.402076, -17.779093],
- *  [177.38079, -17.803937],
- *  [177.40242, -17.826164],
- *  [177.438468, -17.824857],
- *  [177.454948, -17.796746],
- *  [177.434692, -17.77517]
- * ]]);
+ * var poly = {
+ *   "type": "Feature",
+ *   "properties": {},
+ *   "geometry": {
+ *     "type": "Polygon",
+ *     "coordinates": [[
+ *       [177.434692, -17.77517],
+ *       [177.402076, -17.779093],
+ *       [177.38079, -17.803937],
+ *       [177.40242, -17.826164],
+ *       [177.438468, -17.824857],
+ *       [177.454948, -17.796746],
+ *       [177.434692, -17.77517]
+ *     ]]
+ *   }
+ * };
  *
  * var points = turf.explode(poly);
  *
